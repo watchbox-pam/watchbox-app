ff0a1d4e3a95f1be0c2906313f7d796142ab5993
Fabien Arnal <fab.arnal@free.fr>
2025-03-20 13:58:29 +0100

    FIX: add carousels on HomeScreen - add styles to HomeScreen

--- src/components/CarouselPoster.tsx
+++ src/components/CarouselPoster.tsx
@@ -1,25 +1,29 @@
-import React from "react";
 import { FlatList, StyleSheet, View, Image } from "react-native";
 import { Link } from "expo-router";
 
-export default function CarouselPoster({ providers }: { providers: any }) {
+export default function CarouselPoster({ data }: any) {
+	// Vérifier si les données sont disponibles
+	if (!data || data.length === 0) {
+		return <View style={styles.container} />;
+	}
+
 	return (
 		<View style={styles.container}>
 			<FlatList
-				data={providers}
+				data={data}
 				horizontal
 				showsHorizontalScrollIndicator={false}
-				keyExtractor={(item, index) => index.toString()}
+				keyExtractor={(item) => item.id.toString()}
 				renderItem={({ item }) => (
 					<Link
 						style={styles.imageContainer}
 						href={{
 							pathname: "/movie/[id]",
-							params: { id: "1" }
+							params: { id: item.id.toString() }
 						}}>
 						<Image
 							source={{
-								uri: `https://image.tmdb.org/t/p/w500${item}`
+								uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`
 							}}
 							style={styles.image}
 							resizeMode="cover"
@@ -33,16 +37,16 @@ export default function CarouselPoster({ providers }: { providers: any }) {
 
 const styles = StyleSheet.create({
 	image: {
-		width: 100, // 🔥 Correction ici : largeur fixe au lieu de "100%"
-		height: 150, // 🔥 Ajustement pour garder un ratio 2:3
-		borderRadius: 10
+		width: 100,
+		height: 150,
+		borderRadius: 5
 	},
 	imageContainer: {
-		marginRight: 10,
-		alignItems: "center" // ✅ Centrer l'image dans son conteneur
+		marginRight: 5,
+		alignItems: "center"
 	},
 	container: {
-		height: 160, // ✅ Ajuste pour éviter tout dépassement
+		height: 160,
 		paddingLeft: 10
 	}
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,51 @@
+import { useEffect, useState } from "react";
 import CarouselPoster from "../components/CarouselPoster";
 import LogoButton from "../components/Logo";
 import { Text, View, StyleSheet, ScrollView, Platform } from "react-native";
+import { ApiHelper } from "../utils/axios";
+
+import StyledText from "@/src/components/StyledText";
+import styles from "@/src/styles/HomeStyle";
 
 export default function HomeScreen() {
-	const providers = [
-		"/pbpMk2JmcoNnQwx5JGpXngfoWtp.jpg",
-		"/pvske1MyAoymrs5bguRfVqYiM9a.jpg",
-		"/pbpMk2JmcoNnQwx5JGpXngfoWtp.jpg",
-		"/pvske1MyAoymrs5bguRfVqYiM9a.jpg",
-		"/pbpMk2JmcoNnQwx5JGpXngfoWtp.jpg"
-	];
+	const [popularDay, setPopularDay] = useState();
+	const [popularWeek, setPopularWeek] = useState();
+
+	useEffect(() => {
+		const fetchPopularDay = async () => {
+			try {
+				const time_window = "day";
+				const data = await ApiHelper.get(
+					`movies/popular/${time_window}?page=1`
+				);
+				setPopularDay(data);
+			} catch (error) {
+				console.error(
+					"Erreur lors de la récupération des films populaires:",
+					error
+				);
+			}
+		};
+		fetchPopularDay();
+	}, []);
+
+	useEffect(() => {
+		const fetchPopularWeek = async () => {
+			try {
+				const time_window = "week";
+				const data = await ApiHelper.get(
+					`movies/popular/${time_window}?page=1`
+				);
+				setPopularWeek(data);
+			} catch (error) {
+				console.error(
+					"Erreur lors de la récupération des films populaires:",
+					error
+				);
+			}
+		};
+		fetchPopularWeek();
+	}, []);
 
 	return (
 		<ScrollView
@@ -17,51 +53,36 @@ export default function HomeScreen() {
 			contentContainerStyle={styles.contentContainer}
 			showsVerticalScrollIndicator={false}>
 			<View style={styles.header}>
-				<Text style={styles.TitleHeader}>Bonjour Julien-QTX</Text>
+				<StyledText style={styles.TitleHeader}>
+					Bonjour Julien-QTX !
+				</StyledText>
 				<LogoButton />
 			</View>
 
 			<View style={styles.WatchList}>
-				<Text style={styles.TitleWatchList}>WatchList Film</Text>
-				<CarouselPoster providers={providers} />
+				<View style={styles.TitleWatchList}>
+					<StyledText style={styles.MainTitleWatchList}>
+						Popular this day{" "}
+						<StyledText style={styles.SubTitleWatchList}>
+							films
+						</StyledText>
+					</StyledText>
+				</View>
+				{popularDay && <CarouselPoster data={popularDay["results"]} />}
+			</View>
+			<View style={styles.WatchList}>
+				<View style={styles.TitleWatchList}>
+					<StyledText style={styles.MainTitleWatchList}>
+						Popular this week{" "}
+						<StyledText style={styles.SubTitleWatchList}>
+							films
+						</StyledText>
+					</StyledText>
+				</View>
+				{popularWeek && (
+					<CarouselPoster data={popularWeek["results"]} />
+				)}
 			</View>
 		</ScrollView>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#0A1E38",
-		margin: 0,
-		padding: 0
-	},
-	contentContainer: {
-		alignItems: "center",
-		paddingVertical: 20
-	},
-	header: {
-		width: "100%",
-		flexDirection: "row",
-		justifyContent: "space-between",
-		paddingHorizontal: 25,
-		marginTop: Platform.OS === "ios" ? "30" : "0"
-	},
-	TitleHeader: {
-		color: "#ffffff",
-		fontSize: 24,
-		fontWeight: "bold",
-		marginTop: 10
-	},
-	WatchList: {
-		width: 350,
-		marginBottom: 10
-	},
-	TitleWatchList: {
-		color: "#ffffff",
-		fontSize: 18,
-		fontWeight: "bold",
-		marginBottom: 10,
-		marginLeft: 10
-	}
-});
