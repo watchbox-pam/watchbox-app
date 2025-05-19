13f63cb31e7eb62abb5913d79343c8ff943e6be4
Adam <adamboissy2004@gmail.com>
2025-05-19 15:16:40 +0200

    :lipstick: add redirection

---
+++ src/styles/DropDownButton.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		position: "absolute",
+		top: 20,
+		left: 40,
+		zIndex: 2
+	},
+	menu: {
+		position: "absolute",
+		backgroundColor: "#313131",
+		top: 10,
+		left: -110,
+		zIndex: 5
+	},
+	logoutText: {
+		color: "#FF6B6B"
+	}
+});
+
+export default styles;
