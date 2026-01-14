5d5425f4616c3c8a4434943345e50ad9cdd1c934
Adam <adamboissy2004@gmail.com>
2026-01-14 10:38:11 +0100

    fix bug error bar that disappears when exiting the app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,15 @@
 import { Slot } from "expo-router";
-import { View, StatusBar } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import styles from "@/src/styles/LayoutStyle";
 
 export default function Root() {
 	return (
 		<SafeAreaProvider>
-			<StatusBar
-				barStyle="light-content"
-				backgroundColor="#0A1E38"
-				translucent={false}
-			/>
-			<View style={styles.layout}>
+			<StatusBar translucent={true} />
+			<SafeAreaView style={styles.layout}>
 				<Slot />
-			</View>
+			</SafeAreaView>
 		</SafeAreaProvider>
 	);
 }
