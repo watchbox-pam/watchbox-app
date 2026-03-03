bf7a3f0e07705a0d42d6b16885f720c8496e823f
Julien-QTX <julien.quatravaux@live.fr>
2026-03-03 21:01:23 +0100

    fin du bug git poster film homepage impeccable

--- src/styles/CarouselPosterStyle.ts
+++ src/styles/CarouselPosterStyle.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
 	image: {
 		width: 100,
-		height: 150,
+		aspectRatio: 2 / 3,
 		borderRadius: 5
 	},
 	imageContainer: {
