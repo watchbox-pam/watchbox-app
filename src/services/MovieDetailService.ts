7959c0a23ebbf56424166011f3bdfc07f0342c9d
Julien-QTX <julien.quatravaux@live.fr>
2026-04-10 10:47:20 +0200

    Delete Console.log

--- src/services/MovieDetailService.ts
+++ src/services/MovieDetailService.ts
@@ -1,5 +1,11 @@
 import { ApiHelper } from "@/src/utils/axios";
 
+/**
+ * Fetch detailed information about a specific movie by its ID.
+ * @param movieId The unique identifier of the movie.
+ * @returns An object containing success status and movie data or an error message.
+ */
+
 export const fetchMovieDetails = async (movieId: number) => {
 	try {
 		const data: { success: boolean; data: any } = await ApiHelper.get(
