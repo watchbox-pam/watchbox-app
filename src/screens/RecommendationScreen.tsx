c9d4f8d6088f5d3c99deb2f0db926b74ddff3b1c
Adam <adamboissy2004@gmail.com>
2026-05-02 16:59:30 +0200

    fix error

--- src/screens/RecommendationScreen.tsx
+++ src/screens/RecommendationScreen.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback, useRef } from "react";
+import { useState, useCallback, useRef } from "react";
 import { Animated, StatusBar, View, ActivityIndicator } from "react-native";
 
 import styles from "@/src/styles/RecommendationScreenStyle";
