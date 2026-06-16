4b30bd6ebfe0865c16d93b32613f98731457f302
Julien-QTX <julien.quatravaux@live.fr>
2026-06-16 16:30:30 +0200

    fix: path icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import IconCalendar from "@/src/components/IconCalendar";
+import IconCalendar from "@/src/components/icons/IconCalendar";
 import styles from "@/src/styles/HeaderStyle";
 
 const Header = ({ title }: { title: string }) => {
